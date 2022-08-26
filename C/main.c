ac6468de5bef5ff47e234d14056ae033f905e2f5
Tom Carbon <tomcarbon@gmail.com>
2022-08-25 18:43:24 -0700

    refinements main.c

--- C/main.c
+++ C/main.c
@@ -1,17 +1,17 @@
-#include "libdogecoin.h"
 #include <stdio.h>
-#include <string.h>
+#include "libdogecoin.h"
 
 #define PRIV_KEY_LENGTH		53
 #define ADDRESS_LENGTH		35
-char wif_privkey[PRIV_KEY_LENGTH+1];
-char p2pkh_pubkey[ADDRESS_LENGTH+1];
 
 int main(int argc, char **argv) {
+char wif_privkey[PRIV_KEY_LENGTH+1];
+char dogecoin_address[ADDRESS_LENGTH+1];
+
 	dogecoin_ecc_start();
 
-	if (generatePrivPubKeypair(wif_privkey, p2pkh_pubkey, 0)) {
-		printf("{\"data\":{\"privkey\":\"%s\",\"pubkey\":\"%s\"}}", wif_privkey, p2pkh_pubkey);
+	if (generatePrivPubKeypair(wif_privkey, dogecoin_address, 0)) {
+		printf("{\"data\":{\"privkey\":\"%s\",\"pubkey\":\"%s\"}}", wif_privkey, dogecoin_address);
 	} else {
 		printf("{\"data\":\"error\":\"Failure from generatePrivPubKeypair()\"}}");
 	}
